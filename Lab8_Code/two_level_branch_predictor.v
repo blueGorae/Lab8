fe9e72b364370f905927fe9def80b50f6553b11f
yoonsu0816 <yoonsu0816@postech.ac.kr>
2019-05-25 01:59:21 +0900

    doing two level predictor

--- Lab8_Code/two_level_branch_predictor.v
+++ Lab8_Code/two_level_branch_predictor.v
@@ -8,24 +8,39 @@ module two_level_branch_predictor(clk, reset_n, input_ip, output_prediction, inp
 
 	reg [0:0] output_reg;
 
+	reg [1:0] state;
+
 	// you can add more variables
 
 	assign output_prediction = output_reg;
 
 	initial begin
 		output_reg <= 0;
+		state <= 2'b00;
 	end
 
 
 	always @ (*) begin
+		case(state)
+			2'b00 : output_reg = input_taken ? 0 : 0; 
+			2'b01 : output_reg = input_taken ? 1 : 0;
+			2'b10 : output_reg = input_taken ? 1 : 0;
+			2'b11 : output_reg = input_taken ? 1 : 1;
+
 	end
 
 	always @ (negedge reset_n) begin
 		// reset all state asynchronously
 		output_reg <= 0;
+		state <= 2'b00;
 	end
 
 	always @ (posedge clk) begin
+		case(state)
+				2'b00 : state = input_taken ? 2'b01 : 2'b00; 
+				2'b01 : state = input_taken ? 'b10 : 'b00;
+				2'b10 : state = input_taken ? 'b11 : 'b01;
+				2'b11 : state = input_taken ? 'b11 : 'b10;
 	end
 
 endmodule
