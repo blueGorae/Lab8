a5b42d2c12b42da0012e312aea9bc4405e57bf9c
unknown <yoonsu0816@postech.ac.kr>
2019-05-25 16:26:51 +0900

    add table in 2 level predictor

--- Lab8_Code/two_level_branch_predictor.v
+++ Lab8_Code/two_level_branch_predictor.v
@@ -1,3 +1,4 @@
+
 module two_level_branch_predictor(clk, reset_n, input_ip, output_prediction, input_taken);
 	input clk;
 	input reset_n;
@@ -8,39 +9,65 @@ module two_level_branch_predictor(clk, reset_n, input_ip, output_prediction, inp
 
 	reg [0:0] output_reg;
 
-	reg [1:0] state;
+	reg [66:0] branch_table [0:255];
+	reg [1 : 0] state;
+
+
 
 	// you can add more variables
 
 	assign output_prediction = output_reg;
+	integer i;
 
 	initial begin
 		output_reg <= 0;
 		state <= 2'b00;
+		for(i = 0; i < 256; i = i+ 1) begin
+			branch_table[i] = 67'b0;
+		end
 	end
 
 
 	always @ (*) begin
+		for(i = 0; i < 256; i = i+1) begin
+			if((input_ip == branch_table[i][66 : 3]) && branch_table[i][2]) begin
+				state = branch_table[i][1:0];
+				break;
+			end
+		end
+		for(i = 0; i < 256; i = i+1) begin
+			if(branch_table[i][2] == 0) begin
+				branch_table[i][66 : 3] = input_ip;
+				state = 2'b00;
+				brach_table[i][2] = 1;
+				break;
+			end
+		end
 		case(state)
 			2'b00 : output_reg = input_taken ? 0 : 0; 
 			2'b01 : output_reg = input_taken ? 1 : 0;
 			2'b10 : output_reg = input_taken ? 1 : 0;
 			2'b11 : output_reg = input_taken ? 1 : 1;
 		endcase
+
 	end
 
 	always @ (negedge reset_n) begin
 		// reset all state asynchronously
 		output_reg <= 0;
 		state <= 2'b00;
+		for(i = 0; i < 256; i = i+ 1) begin
+			branch_table[i] = 67'b0;
+		end
 	end
 
 	always @ (posedge clk) begin
 		case(state)
-				2'b00 : state = input_taken ? 2'b01 : 2'b00; 
-				2'b01 : state = input_taken ? 'b10 : 'b00;
-				2'b10 : state = input_taken ? 'b11 : 'b01;
-				2'b11 : state = input_taken ? 'b11 : 'b10;
+			2'b00 : state = input_taken ? 2'b01 : 2'b00; 
+			2'b01 : state = input_taken ? 2'b10 : 2'b00;
+			2'b10 : state = input_taken ? 2'b11 : 2'b01;
+			2'b11 : state = input_taken ? 2'b11 : 2'b10;
+		endcase
 	end
 
 endmodule
