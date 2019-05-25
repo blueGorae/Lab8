3de6d8ea0bc452e1e615205b0d9e72891853f67d
seongtae0205 <seongtae0205@postech.ac.kr>
2019-05-25 17:25:46 +0900

    two level done

--- Lab8_Code/two_level_branch_predictor.v
+++ Lab8_Code/two_level_branch_predictor.v
@@ -16,15 +16,18 @@ module two_level_branch_predictor(clk, reset_n, input_ip, output_prediction, inp
 
 
 
+
 	// you can add more variables
 
 	assign output_prediction = output_reg;
 	integer i;
 	reg is_hit;
+	reg entry_added;
 
 	initial begin
 		is_hit <= 0 ;
 		output_reg <= 0;
+		entry_added <= 0;
 		state <= 2'b00;
 		prev_ip <= 64'bz;
 		for(i = 0; i < 256; i = i+ 1) begin
@@ -47,44 +50,46 @@ module two_level_branch_predictor(clk, reset_n, input_ip, output_prediction, inp
 		
 		is_hit <= 0 ;
 		output_reg <= 0;
+		entry_added <= 0;
 		state <= 2'b00;
 		prev_ip <= 64'bz;
 
 		for(i = 0; i < 256; i = i+ 1) begin
 			branch_table[i] = 67'b0;
 		end
+
+		$display("reset_n activated");
 	end
 
 	always @ (posedge clk) begin
 		is_hit = 0 ;
+		entry_added = 0;
 
 		for(i = 0; i < 256; i = i+1) begin
 			if((input_ip == branch_table[i][66 : 3]) && branch_table[i][2]) begin
 				state = branch_table[i][1:0];
 				is_hit = 1;
-				break;
 			end
 		end
 		if(!is_hit) begin
 			for(i = 0; i < 256; i = i+1) begin
-				if(branch_table[i][2] == 0) begin
+				if(branch_table[i][2] == 0 && !entry_added) begin
 					branch_table[i][66 : 3] = input_ip;
 					state = 2'b00;
-					brach_table[i][2] = 1;
-					break;
+					branch_table[i][2] = 1;
+					entry_added = 1;
 				end
 			end
 		end
 
 		for(i = 0; i < 256; i = i+1) begin
 			if((prev_ip == branch_table[i][66 : 3]) && branch_table[i][2]) begin
-				case (branch_table[i][1:0]) begin
+				case (branch_table[i][1:0])
 					2'b00 : branch_table[i][1:0] = input_taken ? 2'b01 : 2'b00;
 					2'b01 : branch_table[i][1:0] = input_taken ? 2'b10 : 2'b00;
 					2'b10 : branch_table[i][1:0] = input_taken ? 2'b11 : 2'b01;
 					2'b11 : branch_table[i][1:0] = input_taken ? 2'b11 : 2'b10;
 				endcase		
-				break;
 			end
 		end
 
